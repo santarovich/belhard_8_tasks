c7e5fc3650980b9655cdceea673074452468d0aa
Vyacheslav Rineisky <rineisky@gmail.com>
2021-09-23 19:41:49 +0300

    update_tasks

--- tasks/easy/inheritance_polimorphism/duck_typing.py
+++ tasks/easy/inheritance_polimorphism/duck_typing.py
@@ -1,14 +1,21 @@
 """
 Создать 3 класса:
 
-Cat, Duck, Cow
+AmericanPerson, RussianPerson, GermanyPerson
 
-в каждом классе определить метод says()
+в каждом классе определить метод i_love_science()
 
-Cat.says() - кошка говорит мяу
-Duck.says() - утка говорит кря
-Cow.says() - корова говорит муу
+AmericanPerson.i_love_science() -> "I love science"
+RussianPerson.i_love_science() - "Я люблю науку"
+GermanyPerson.i_love_science() - "ich liebe Wissenschaft"
 
 
-Написать функцию animal_says(), которая принимает объект и вызывает метод says
+Написать функцию person_love_science, которая принимает объект и вызывает метод
+i_love_science. Функция должна возвращать строку вида
+"{obj.__class__.__name__} says that: {obj.i_love_science()}"
+
+В блоке if __name__ == "__main__": сделать объекты трех классов и по очереди
+передать их в функцию person_love_science.
+
+https://www.youtube.com/watch?v=8o7ZKTvZpLc
 """
