f795f4919c01caa121bab8c5022c0458d36846bc
Olga Santarovich <santarovich@gmail.com>
2022-03-11 15:30:07 +0300

    easy/decorators/class_benchmark

--- tasks/easy/decorators/class_benchmark.py
+++ tasks/easy/decorators/class_benchmark.py
@@ -15,8 +15,45 @@
 Время окончания: {end_time}
 Всего затрачено времени на выполнение: {end_time - start_time}
 """
+from functools import wraps
 import time
 
 # start_time = time.time()
 # end_time = time.time()
 # difference = e - s
+
+
+def def_benchmark(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"Выполняем {func.__name__} с args: {args} и kwargs: {kwargs}\n"
+              f"Время начала: {start_time}")
+        # end_time = time.time()
+        print(f"Выполнено {func.__name__}\n"
+              f"Время окончания: {end_time}\n"
+              f"Всего затрачено времени на выполнение: {end_time - start_time}")
+        return result
+    return wrapper
+
+
+def class_benchmark(cls):
+    functions = {
+        name: value for name, value
+        in cls.__dict__.items()
+        if callable(value) and not name.startswith("_")
+    }
+    for name, func in functions.items():
+        func_with_dec = def_benchmark(func)
+        setattr(cls, name, func_with_dec)
+    return cls
+
+
+@class_benchmark
+class Benchmark:
+    attribute = 1
+
+    def function(self):
+        return 1 + self.attribute
